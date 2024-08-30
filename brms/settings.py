93bf61f8e1d9465af4f27e9ded472640dc4b7071
fredrickmugish <fredrickgotfried@gmail.com>
2024-08-30 23:23:05 +0000

    Another commit

--- brms/settings.py
+++ brms/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [BASE_DIR/"static"]
 STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles/'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- system/urls.py
+++ system/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
@@ -14,3 +16,6 @@ urlpatterns = [
     path('site_courses/', views.site_courses, name="site_courses"),
     path('contact/', views.contact, name="contact"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
