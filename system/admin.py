01d8d2e352aeb46b3a0fd538f93b12ce41b13289
fredrickmugish <fredrickgotfried@gmail.com>
2024-08-31 15:43:12 +0000

    Another commit

--- system/admin.py
+++ system/admin.py
@@ -4,11 +4,13 @@ from django.utils.html import format_html
 # Register your models here.
 
 class CourseAdmin(admin.ModelAdmin):
-  list_display = ('title', 'duration', 'fee')
-admin.site.register(Course,CourseAdmin)
+    list_display = ('title', 'duration', 'fee')
+    search_fields = ('title', 'duration', 'fee')  
+admin.site.register(Course, CourseAdmin)
 
 class RegistrationAdmin(admin.ModelAdmin):
   list_display = ('full_name', 'course', 'view_application_form')
+  search_fields = ('full_name', 'course__title') 
   def view_application_form(self, obj):
         if obj.application_form:
             return format_html('<a href="{}" target="_blank">View Form</a>', obj.application_form.url)
